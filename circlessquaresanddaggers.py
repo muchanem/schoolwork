198f324cab4f9358378818f2ec4c8b8103771d53
muchanem <muchanem@gmail.com>
2014-12-03 14:00:01 -0500

    Update circlessquaresanddaggers.py

--- circlessquaresanddaggers.py
+++ circlessquaresanddaggers.py
@@ -5,6 +5,11 @@ zombie = codestudio.load('s1level82')
 z = zombie
 
 def draw_circle(self)
-z.turn_right(90)
+z.turn_right(90) print("Simalarites")
+print('')
 z.move_forward(50)
 z.turn_left(90)
+
+
+
+print('')
